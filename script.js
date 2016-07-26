2ac74800904e061867034ecf2726420b5ed23d7d
Alan <wangchaoallan@gmail.com>
2016-07-26 20:49:32 +1000

    finish body
    finish body,load photos

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+
+for (var i =1; i<13; i++ ){
+
+	var img = "img/"+i+".jpg";
+	var image = "<img src="+img+">";
+	$('.photo').append("<div>"+image+"</div>");	
+
+}
+
+$('.photo').find('div').addClass('photoFrame');
+
+$('.photoFrame').find('img').addClass('photoSize');
+
+
+$('.photo').find('div').hover(function(){
+
+$(this).addClass('mousehover');
+},function(){
+$(this).removeClass('mousehover');
+});
+});
+
+
