cb0f07195f5e73b63d2e64e19dd72aa323576d3a
Alan <wangchaoallan@gmail.com>
2016-07-27 13:02:32 +1000

    Added hover effect
    add hover effect on Photos, however it is hard coded in HTML , not in
    jQuery

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 
 
 for (var i =1; i<13; i++ ){
 
 	var img = "img/"+i+".jpg";
 	var image = "<img src="+img+">";
+	var text ="text goes here";
 	$('.photo').append("<div>"+image+"</div>");	
 
 }
@@ -23,3 +24,4 @@ $(this).removeClass('mousehover');
 });
 
 
+*/
